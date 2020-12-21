7c11945a28e5507cb3630577f39d1eecdecc1c0f
talldonkey <6707639+talldonkey@users.noreply.github.com>
2020-12-21 02:10:14 -0800

    BMG opposite direction of Titan, Disable Beeper

--- Marlin/src/pins/lpc1768/pins_BTT_SKR_common.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_common.h
@@ -94,7 +94,7 @@
 //
 #if HAS_WIRED_LCD && DISABLED(LCD_USE_I2C_BUZZER)
   #define BEEPER_PIN                       P1_30  // (37) not 5V tolerant
-#endif
+  #define BEEPER_PIN                       -1  // Disable Beeper
 
 //
 // SD Support
